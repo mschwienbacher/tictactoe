28f49baee9e6318fcf2fec030826cb2aa3d96966
Michael Schwienbacher <schwimmii@hotmail.com>
2023-02-22 15:02:52 +0100

    Show Elements

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let fields = [];
 let currentShape = "cross";
 
-function fillShape(id) { 
-    
+function fillShape(id) {  
     if(currentShape == "cross") {
         currentShape = "circle";
     } else {
@@ -10,5 +9,16 @@ function fillShape(id) {
     }
 
     fields[id] = currentShape;
-    console.log(fields);
+    draw();
+}
+
+function draw() {
+    for(let i = 0; i < fields.length; i++) {
+        if(fields[i] == "circle") {
+            document.getElementById("circle-" + i).classList.remove("d-none");
+        }
+        if(fields[i] == "cross") {
+            document.getElementById("cross-" + i).classList.remove("d-none");
+        }
+    }
 }
