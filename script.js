084c6aa6b550b8135c5d312029092a846e8df8b8
Michael Schwienbacher <schwimmii@hotmail.com>
2023-03-13 13:59:52 +0100

    Restyling v2

--- script.js
+++ script.js
@@ -2,38 +2,23 @@ let fields = [];
 let gameOver = false;
 let currentShape = "cross";
 
+
 function fillShape(id) {  
     if(!fields[id] && !gameOver) {
         if(currentShape == "cross") {
                 currentShape = "circle";
-                document.getElementById("player-1").classList.remove("player-inactive");
-                document.getElementById("player-2").classList.add("player-inactive");
+                document.getElementById("player-1").classList.add("player-inactive");
+                document.getElementById("player-2").classList.remove("player-inactive");
             } else {
                 currentShape = "cross";
-                document.getElementById("player-2").classList.remove("player-inactive");
-                document.getElementById("player-1").classList.add("player-inactive");
+                document.getElementById("player-2").classList.add("player-inactive");
+                document.getElementById("player-1").classList.remove("player-inactive");
             }
 
             fields[id] = currentShape;
             draw();
             checkForWin();
         }
-}
-
-function restart() {
-    gameOver = false;
-    fields = [];
-
-    document.getElementById("game-is-over").classList.add("d-none");
-    document.getElementById("restart-button").classList.add("d-none");
-    for(let i=1; i < 8; i++) {
-        document.getElementById("line-" + i).classList.add("d-none");
-    }
-
-    for(let i=0; i < 9; i++) {
-        document.getElementById("circle-" + i).classList.add("d-none");
-        document.getElementById("cross-" + i).classList.add("d-none");
-    }
 }
 
 function draw() {
@@ -47,10 +32,26 @@ function draw() {
     }
 }
 
+function restart() {
+    gameOver = false;
+    fields = [];
+
+    document.getElementById("game-is-over").classList.add("d-none");
+    document.getElementById("restart-button").classList.add("d-none");
+
+    for(let i = 1; i < 8; i++) {
+        document.getElementById("line-" + i).classList.add("d-none");
+    }
+
+    for(let i = 0; i < 9; i++) {
+        document.getElementById("circle-" + i).classList.add("d-none");
+        document.getElementById("cross-" + i).classList.add("d-none");
+    }
+}
+
 function checkForWin() {
     let winner;
 
-    //First row
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
         document.getElementById("line-1").style.transform = "scaleX(1.0)";
@@ -82,8 +83,6 @@ function checkForWin() {
         document.getElementById("line-6").style.transform = "rotate(90deg) scaleX(1.0)";
     }
 
-    //
-
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
         document.getElementById("line-7").style.transform = "rotate(45deg) scaleX(1.2)";
@@ -94,11 +93,17 @@ function checkForWin() {
         document.getElementById("line-8").style.transform = "rotate(-45deg) scaleX(1.2)";
     }
 
+
     if(winner) {
-        //console.log("GEWONNEN: ", winner);
         gameOver = true;
         setTimeout(function(){
             document.getElementById("game-is-over").classList.remove("d-none");
+            if(winner == "circle") {
+                document.getElementById("thewinner").innerHTML = '<strong class="p1">PLAYER 1</strong> is the WINNER!';
+            } else {
+                document.getElementById("thewinner").innerHTML = '<strong class="p2">PLAYER 2</strong> is the WINNER!';
+            }
+            
             document.getElementById("restart-button").classList.remove("d-none");
         }, 600);
        
